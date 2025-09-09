e9a23cfaf91ab44769ac4ad619a0a3a181f7f368
GABRIEL JOSÉ SANTOS DA SILVA <25132745@CFP127ALUNO.LOCAL>
2025-09-09 11:28:21 -0300

    js feito

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 'use strict'
 
 async function buscarImagens(raca) {
-    const url = `https://dog.ceo/api/breed/${raca}/images/`;
+    const url = `https://dog.ceo/api/breed/${raca}/images`;
     const response = await fetch(url);
     const imagens = await response.json();
-    console.log(imagens.message);
     return imagens.message;
 }
 
-buscarImagens('african');
+function criarCard(imagem) {
+    const container = document.querySelector('.fotos');
+    const containerFoto = document.createElement('div');
+
+    containerFoto.classList.add('container-foto', 'glass-card');
+
+    const img = document.createElement('img');
+    img.src = imagem;
+
+    container.appendChild(containerFoto);
+    containerFoto.appendChild(img);
+}
+
+async function mostrarImagens(raca) {
+    const imagens = await buscarImagens(raca);
+
+    const container = document.querySelector('.fotos');
+    container.innerHTML = '';
+
+    imagens.forEach(imagem => {
+        criarCard(imagem);
+    });
+}
+
+
+
+const campoPesquisa = document.querySelector('.campo');
+const botaoPesquisa = document.querySelector('.botao');
+
+botaoPesquisa.addEventListener('click', () => mostrarImagens(campoPesquisa.value));
